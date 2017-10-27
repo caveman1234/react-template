fcb6623cbb7a01617d2b4683e2201cbb158e1b5b
diaoxinrui <443135581@qq.com>
2017-10-27 18:58:22 +0800

    react 模版

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.scss';
+export default class App extends Component {
+  state = {
+    shopCount: 0
+  }
+  render() {
+    return (
+      <div className="app">
+        <div>app</div>
+      </div>
+    )
+  }
+}
+
+
+
+
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+
+const test = (state = 'test', action) => ({});
+
+export default combineReducers({
+    test
+});
